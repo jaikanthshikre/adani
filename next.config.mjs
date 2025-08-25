3cdf4082a56e7c9e6a241a1bf27f2aa216c7d382
jaikanthshikre <jaikanthshikre36@gmail.com>
2025-08-25 13:08:11 +0530

    fnupdate

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",   // 👈 tells Next.js to build into /out
+   
   images: {
     unoptimized: true, // 👈 disables image optimization (needed for static export)
   },
